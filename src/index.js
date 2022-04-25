bb95d198ea7af24976939adeada9e40e03d5976c
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-04-25 08:47:15 -0300

    style code refactor

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) =>
-    response.json({ message: "Hello World Ingite!" }
-    ));
+    response.json({ message: "Hello World Ingite Node JS!" })
+);
 
 app.listen(3333);
